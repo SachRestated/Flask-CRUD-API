b4ad500acc0ba5e9a5eeb3ca0026953ce3919be9
SachRestated <sachinvashisht1999@gmail.com>
2020-12-02 20:28:15 +0530

    Deploy to Heroku

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 app.secret_key = 'Sachin'
 
+db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
@@ -33,6 +34,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
-db.init_app(app)
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+app.run(port=5000, debug=True)
+    
