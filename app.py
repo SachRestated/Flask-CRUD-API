26e71bf31616e1d1ca1c289b18b1ed8c082074ff
SachRestated <sachinvashisht1999@gmail.com>
2020-12-02 20:41:37 +0530

    Deploy to Heroku

--- app.py
+++ app.py
@@ -6,33 +6,29 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
-from db import db 
-
-# import create_table 
+from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
 api = Api(app)
-app.secret_key = 'Sachin'
 
-db.init_app(app)
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity)
 
-# https://www.flaskapi.org/api-guide/status-codes/
-# how status codes are returned in simple flask app 
-# without Flask_restful
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
-    
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
